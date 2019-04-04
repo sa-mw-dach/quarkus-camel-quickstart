6157b436839fa1775fdd2f53f8dc9b501ebc144e
Romain Pelisse <belaran@gmail.com>
2019-04-04 09:59:55 +0200

    Adding some violations to GraalVM limitations for demo purpose.

--- src/main/java/io/quarkus/CamelRouteBuilder.java
+++ src/main/java/io/quarkus/CamelRouteBuilder.java
@@ -1,11 +1,22 @@
 package io.quarkus;
 
 import org.apache.camel.builder.RouteBuilder;
+import sun.misc.Unsafe;
+
 
 public class CamelRouteBuilder extends RouteBuilder {
+
+	{
+        // Not supported (fully) by GraalVM!
+	}
 
     public void configure() throws Exception {
         from("timer:test")
         .log("Test 123");
+    }
+
+    public void finalize() {
+        // GraalVM will never call this!
+        Unsafe unsafe = null;
     }
 }
